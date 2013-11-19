b43f0adc3e0765f33212db0477d188e21c2c9c3e
pineauk <kylepineau@Kyles-Mac.local>
2013-11-19 11:27:29 -0400

    Working Agenda

--- UPEI Planner/CalendarViewController.h
+++ UPEI Planner/CalendarViewController.h
@@ -2,12 +2,21 @@
 //  CalendarViewController.h
 //  UPEI Planner
 //
-//  Created by Evan Jackson on 2013-11-04.
+//  Created by Kyle Pineau on 2013-11-19.
 //  Copyright (c) 2013 Kyle Pineau & Evan Jackson. All rights reserved.
 //
 
 #import <UIKit/UIKit.h>
+#import "Exam.h"
+#import "AppDelegate.h"
+#import "StudentClass.h"
 
-@interface CalendarViewController : UIViewController
+@interface CalendarViewController : UITableViewController
 
+@property (strong, nonatomic) NSMutableArray *exams;
+@property StudentClass *course;
+@property int rowNumber;
+@property (strong, nonatomic) NSArray *classes;
+@property (strong, nonatomic) NSString *classID;
+@property (strong, nonatomic) NSMutableArray *examList;
 @end
--- UPEI Planner/CalendarViewController.m
+++ UPEI Planner/CalendarViewController.m
@@ -2,7 +2,7 @@
 //  CalendarViewController.m
 //  UPEI Planner
 //
-//  Created by Evan Jackson on 2013-11-04.
+//  Created by Kyle Pineau on 2013-11-19.
 //  Copyright (c) 2013 Kyle Pineau & Evan Jackson. All rights reserved.
 //
 
@@ -13,27 +13,182 @@
 @end
 
 @implementation CalendarViewController
-
-- (id)initWithNibName:(NSString *)nibNameOrNil bundle:(NSBundle *)nibBundleOrNil
+@synthesize examList;
+- (id) init
 {
-    self = [super initWithNibName:nibNameOrNil bundle:nibBundleOrNil];
+    self = [super initWithStyle:UITableViewStyleGrouped];
+    return self;
+}
+
+- (id)initWithStyle:(UITableViewStyle)style
+{
+    self = [super initWithStyle:style];
     if (self)
     {
-        [[self tabBarItem] setTitle:@"Calendar"];
+        [self setTitle:@"Agenda"];
     }
     return self;
 }
 
 - (void)viewDidLoad
 {
+    [[self tableView] setDataSource:self];
+    [[self tableView] setDelegate:self];
     [super viewDidLoad];
-    // Do any additional setup after loading the view from its nib.
+    examList = [[NSMutableArray alloc]init];
+    [[self navigationItem] setTitle:@"Exams"];
+
 }
 
 - (void)didReceiveMemoryWarning
 {
     [super didReceiveMemoryWarning];
     // Dispose of any resources that can be recreated.
+}
+
+#pragma mark - Table view data source
+
+- (NSInteger)numberOfSectionsInTableView:(UITableView *)tableView
+{
+#warning Potentially incomplete method implementation.
+    // Return the number of sections.
+    return 3;
+}
+
+- (NSInteger)tableView:(UITableView *)tableView numberOfRowsInSection:(NSInteger)section
+{
+    NSLog(@"Number of exams: %d", [examList count]);
+    if(section == 0)
+        return [examList count];
+    return 0;
+}
+- (NSString *)tableView:(UITableView *)tableView titleForHeaderInSection:(NSInteger)section
+{
+    if(section == 0)
+    {
+        return @"Exams";
+    }
+    else if(section == 1)
+    {
+        return @"Assignments";
+    }
+    else if(section == 2)
+    {
+        return @"Projects";
+    }
+}
+- (UITableViewCell *)tableView:(UITableView *)tableView cellForRowAtIndexPath:(NSIndexPath *)indexPath
+{
+    static NSString *cellId = @"AgendaCell";
+    UITableViewCell *cell = [tableView dequeueReusableCellWithIdentifier:cellId];
+    if (cell == nil)
+    {
+        cell = [[UITableViewCell alloc] initWithStyle:UITableViewCellStyleSubtitle reuseIdentifier:cellId];
+    }
+    
+    // Configure the cell...
+    cell.accessoryType = UITableViewCellAccessoryDetailDisclosureButton;
+    
+    Exam *exam = [examList objectAtIndex:[indexPath row]];
+    [[cell textLabel] setText:[exam name]];
+    [[cell detailTextLabel]setText:[NSString stringWithFormat:@"%@", [exam due_date]]];
+    
+    return cell;
+}
+
+/*
+// Override to support conditional editing of the table view.
+- (BOOL)tableView:(UITableView *)tableView canEditRowAtIndexPath:(NSIndexPath *)indexPath
+{
+    // Return NO if you do not want the specified item to be editable.
+    return YES;
+}
+*/
+
+/*
+// Override to support editing the table view.
+- (void)tableView:(UITableView *)tableView commitEditingStyle:(UITableViewCellEditingStyle)editingStyle forRowAtIndexPath:(NSIndexPath *)indexPath
+{
+    if (editingStyle == UITableViewCellEditingStyleDelete) {
+        // Delete the row from the data source
+        [tableView deleteRowsAtIndexPaths:@[indexPath] withRowAnimation:UITableViewRowAnimationFade];
+    }   
+    else if (editingStyle == UITableViewCellEditingStyleInsert) {
+        // Create a new instance of the appropriate class, insert it into the array, and add a new row to the table view
+    }   
+}
+*/
+
+/*
+// Override to support rearranging the table view.
+- (void)tableView:(UITableView *)tableView moveRowAtIndexPath:(NSIndexPath *)fromIndexPath toIndexPath:(NSIndexPath *)toIndexPath
+{
+}
+*/
+
+/*
+// Override to support conditional rearranging of the table view.
+- (BOOL)tableView:(UITableView *)tableView canMoveRowAtIndexPath:(NSIndexPath *)indexPath
+{
+    // Return NO if you do not want the item to be re-orderable.
+    return YES;
+}
+*/
+- (AppDelegate *)appDelegate {
+    return (AppDelegate *)[[UIApplication sharedApplication] delegate];
+}
+
+// Performs a fetch and reloads the table view.
+- (void) loadTableData {
+    NSManagedObjectContext *context = [[self appDelegate] managedObjectContext];
+    
+    // Construct a fetch request
+    NSFetchRequest *fetchRequest = [[NSFetchRequest alloc] init];
+    NSEntityDescription *entity = [NSEntityDescription entityForName:@"StudentClass"
+                                              inManagedObjectContext:context];
+    [fetchRequest setEntity:entity];
+    
+    // Add an NSSortDescriptor to sort the faculties alphabetically
+    NSSortDescriptor *sortDescriptor = [[NSSortDescriptor alloc] initWithKey:@"name" ascending:YES];
+    NSArray *sortDescriptors = [[NSArray alloc] initWithObjects:sortDescriptor, nil];
+    [fetchRequest setSortDescriptors:sortDescriptors];
+    
+    NSError *error = nil;
+    NSArray *fetchedObjects = [context executeFetchRequest:fetchRequest error:&error];
+    NSLog(@"%i",fetchedObjects.count);
+    for(int i =0; i<fetchedObjects.count;i++) {
+    _classes = [[NSArray alloc] initWithObjects:[fetchedObjects objectAtIndex:i], nil];
+    
+        _course = [_classes objectAtIndex:0];
+        NSLog(@"%i",i );
+    _exams = [NSMutableArray arrayWithArray:[[_course exam] allObjects]];
+        
+    
+        for(int j = 0; j<_exams.count;j++){
+            [examList addObject:[_exams objectAtIndex:j]];
+            NSLog(@"%@",[[_exams objectAtIndex:j] name]);
+        }
+    }
+    [[self tableView] reloadData];
+}
+
+- (void) viewWillAppear:(BOOL)animated
+{
+    [super viewWillAppear:animated];
+    // Load the table data //
+    [self loadTableData];
+}
+#pragma mark - Table view delegate
+
+- (void)tableView:(UITableView *)tableView didSelectRowAtIndexPath:(NSIndexPath *)indexPath
+{
+    // Navigation logic may go here. Create and push another view controller.
+    /*
+     <#DetailViewController#> *detailViewController = [[<#DetailViewController#> alloc] initWithNibName:@"<#Nib name#>" bundle:nil];
+     // ...
+     // Pass the selected object to the new view controller.
+     [self.navigationController pushViewController:detailViewController animated:YES];
+     */
 }
 
 @end
